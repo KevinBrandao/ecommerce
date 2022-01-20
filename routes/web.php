3457f8a5a54ad57ced1c08272912ab2eb88c9f9e
KevinBrandao <kevenbrandaoms@outlook.com>
2022-01-19 22:05:08 -0300

    Crud

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use App\Models\Product;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/products', [
+    ProductController::class, 'index'
+]);
+
+Route::get('/products/create', [
+    ProductController::class, 'create'
+]);
+
+Route::post('/products/store', [
+    ProductController::class, 'store'
+]);
+
+Route::get('/products/show/{id}', [
+    ProductController::class, 'show'
+]);
+
+Route::get('/products/edit/{id}', [
+    ProductController::class, 'edit'
+]);
+
+Route::post('/products/update/{id}', [
+    ProductController::class, 'update'
+]);
+
+Route::get('/products/delete/{id}', [
+    ProductController::class, 'delete'
+]);
+
